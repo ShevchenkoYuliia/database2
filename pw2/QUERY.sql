e1ac526287f7c1a2d7b15a88b958c2cb0710014e
ShevchenkoYuliia <yuliia.shevchenko1845@gmail.com>
2025-03-10 23:21:22 +0200

    pw2

--- pw2/QUERY.sql
+++ pw2/QUERY.sql
@@ -1,23 +1,78 @@
 USE pcmouse;
+-- завдання 3
+SELECT * FROM ComputerMouse WHERE Price < 2000;
 
--- Select all mouse models and their prices
-SELECT ModelName, Price FROM ComputerMouse;
+-- завдання 4
+SELECT * FROM ComputerMouse WHERE Price < 2000 AND Color = 'Gray';
 
--- Count the total stock quantity
-SELECT SUM(StockQuantity) AS TotalStock FROM ComputerMouse;
+--завдання 5
+SELECT * FROM Customer WHERE ShippingAddress LIKE '%Poltava%';
 
--- Show all orders with their details
-SELECT OrderTable.OrderId, Customer.FirstName, Customer.LastName, ComputerMouse.ModelName, 
-       OrderDetails.ProductQuantity, OrderDetails.ItemAmount
-FROM OrderTable
-JOIN Customer ON OrderTable.CustomerId = Customer.CustomerId
-JOIN OrderDetails ON OrderTable.OrderId = OrderDetails.OrderId
-JOIN ComputerMouse ON OrderDetails.ProductId = ComputerMouse.ProductId;
+--завдання 6
+SELECT * FROM OrderTable
+JOIN Customer ON OrderTable.CustomerId = Customer.CustomerId;
 
--- Calculate the total amount of all orders
-SELECT SUM(TotalAmount) AS TotalSum FROM OrderTable;
+--завдання 7
+SELECT 
+    Customer.CustomerId,
+    Customer.FirstName,
+    Customer.LastName,
+    OrderTable.OrderId,
+    OrderTable.OrderDate,
+    OrderDetails.ProductQuantity,
+    ComputerMouse.ModelName,
+    ComputerMouse.Brand,
+    ComputerMouse.Color
+FROM Customer
+LEFT JOIN OrderTable ON Customer.CustomerId = OrderTable.CustomerId
+LEFT JOIN OrderDetails ON OrderTable.OrderId = OrderDetails.OrderId
+RIGHT JOIN ComputerMouse ON OrderDetails.ProductId = ComputerMouse.ProductId;
 
--- Show suppliers that supply wired mice
-SELECT DISTINCT Supplier.* FROM Supplier
+--завдання 8
+SELECT ModelName, Price
+FROM ComputerMouse
+WHERE SupplierId IN (
+    SELECT SupplierId 
+    FROM Supplier 
+    WHERE Address LIKE '%Zhytomyr%'
+)
+AND Price >= 2000;
+
+--завдання 9
+SELECT 
+    Supplier.Name AS SupplierName,
+    ComputerMouse.ModelName,
+    SUM(ComputerMouse.StockQuantity) AS TotalStock
+FROM ComputerMouse
+JOIN Supplier ON ComputerMouse.SupplierId = Supplier.SupplierId
+GROUP BY Supplier.Name, ComputerMouse.ModelName
+HAVING SUM(ComputerMouse.StockQuantity) > 40
+ORDER BY TotalStock DESC;
+
+--завдання 10
+SELECT 
+    Supplier.Name AS SupplierName,
+    ComputerMouse.ModelName,
+    SUM(ComputerMouse.StockQuantity) AS TotalStock,
+    (SELECT SUM(OrderDetails.ProductQuantity) 
+     FROM OrderDetails 
+     WHERE OrderDetails.ProductId = ComputerMouse.ProductId) AS TotalSold
+FROM Supplier
 JOIN ComputerMouse ON Supplier.SupplierId = ComputerMouse.SupplierId
-WHERE ComputerMouse.Type = 'Wired';
+LEFT JOIN OrderDetails ON ComputerMouse.ProductId = OrderDetails.ProductId
+WHERE Supplier.Address LIKE '%Lviv%' 
+GROUP BY Supplier.Name, ComputerMouse.ModelName, ComputerMouse.ProductId
+HAVING SUM(ComputerMouse.StockQuantity) <= 30;
+
+--завдання 11
+SELECT 
+    Supplier.Name AS SupplierName,
+	Supplier.Address AS SupplierAddress,
+    ComputerMouse.ModelName,
+    ComputerMouse.Price,
+    ComputerMouse.Color
+FROM Supplier
+JOIN ComputerMouse ON Supplier.SupplierId = ComputerMouse.SupplierId
+WHERE Supplier.Address LIKE '%Kyiv%' 
+AND ComputerMouse.Price > 2000 
+AND ComputerMouse.Color = 'Black';
