bd932b1532e1a3a609c0f118007fb1abbf067078
ShevchenkoYuliia <yuliia.shevchenko1845@gmail.com>
2025-05-21 00:37:48 +0300

    lab8

---
+++ lab8/Laboratorna08/PCMouseAPP/PCMouseAPP/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PCMouseAPP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtPageNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(780, 485);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txtPageNumber
+            // 
+            this.txtPageNumber.Location = new System.Drawing.Point(78, 9);
+            this.txtPageNumber.Name = "txtPageNumber";
+            this.txtPageNumber.Size = new System.Drawing.Size(21, 20);
+            this.txtPageNumber.TabIndex = 2;
+            this.txtPageNumber.Text = "1";
+            this.txtPageNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "of 5";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.FlatAppearance.BorderSize = 0;
+            this.btnFirst.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFirst.Image = global::PCMouseAPP.Properties.Resources.first;
+            this.btnFirst.Location = new System.Drawing.Point(32, 4);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(22, 27);
+            this.btnFirst.TabIndex = 1;
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.BtnFirst_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.FlatAppearance.BorderSize = 0;
+            this.btnLast.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLast.Image = global::PCMouseAPP.Properties.Resources.last;
+            this.btnLast.Location = new System.Drawing.Point(150, 4);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(22, 27);
+            this.btnLast.TabIndex = 1;
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.BtnLast_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.FlatAppearance.BorderSize = 0;
+            this.btnPrev.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrev.Image = global::PCMouseAPP.Properties.Resources.prev;
+            this.btnPrev.Location = new System.Drawing.Point(50, 4);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(22, 27);
+            this.btnPrev.TabIndex = 2;
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.BtnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.FlatAppearance.BorderSize = 0;
+            this.btnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNext.Image = global::PCMouseAPP.Properties.Resources.next;
+            this.btnNext.Location = new System.Drawing.Point(130, 4);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(22, 27);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.BtnNext_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.FlatAppearance.BorderSize = 0;
+            this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoad.Image = global::PCMouseAPP.Properties.Resources.reload;
+            this.btnLoad.Location = new System.Drawing.Point(256, 4);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(22, 27);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatAppearance.BorderSize = 0;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Image = global::PCMouseAPP.Properties.Resources.add;
+            this.btnAdd.Location = new System.Drawing.Point(284, 4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(22, 27);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.FlatAppearance.BorderSize = 0;
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEdit.Image = global::PCMouseAPP.Properties.Resources.edit;
+            this.btnEdit.Location = new System.Drawing.Point(312, 4);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(22, 27);
+            this.btnEdit.TabIndex = 2;
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.BtnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.FlatAppearance.BorderSize = 0;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Image = global::PCMouseAPP.Properties.Resources.bin;
+            this.btnDelete.Location = new System.Drawing.Point(340, 4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(22, 27);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 532);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.txtPageNumber);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Name = "Form1";
+            this.Text = "PCMouseDatabaseApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private int currentTableIndex = 0;
+        private List<string> tableNames = new List<string> { "Customer", "ComputerMouse", "Supplier", "OrderTable", "OrderDetails" };
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DataTable dataTable;
+        private System.Windows.Forms.TextBox txtPageNumber;
+        private Label label1;
+    }
+    }
+
---
+++ lab8/Laboratorna08/PCMouseAPP/PCMouseAPP/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PCMouseAPP
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source=DESKTOP-C5TOBO7;Initial Catalog=PCMouse;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Dictionary<string, List<string>> tableColumns = new Dictionary<string, List<string>>
+        {
+            { "Customer", new List<string> { "CustomerId", "FirstName", "LastName", "Email", "PhoneNumber", "ShippingAddress", "BonusAccountNumber" } },
+            { "Supplier", new List<string> { "SupplierId", "Name", "Address", "ContactNumber", "Email" } },
+            { "ComputerMouse", new List<string> { "ProductId", "ModelName", "Brand", "Type", "ButtonCount", "Size", "Price", "StockQuantity","Color", "SupplierId" } },
+            { "OrderTable", new List<string> { "OrderId", "CustomerId", "OrderDate", "TotalAmount" } },
+            { "OrderDetails", new List<string> { "OrderDetailsId", "ProductId", "OrderId", "ProductQuantity", "ItemAmount" } }
+        };
+
+        Dictionary<string, string> tablePrimaryKeys = new Dictionary<string, string>
+        {
+            { "Customer", "CustomerId" },
+            { "Supplier", "SupplierId" },
+            { "ComputerMouse", "ProductId" },
+            { "OrderTable", "OrderId" },
+            { "OrderDetails", "OrderDetailsId" }
+        };
+
+        private void LoadCurrentTable()
+        {
+            string tableName = tableNames[currentTableIndex];
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter($"SELECT * FROM {tableName}", conn);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                    this.Text = $"Відображається таблиця: {tableName}";
+                    txtPageNumber.Text = (currentTableIndex + 1).ToString();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Помилка при завантаженні: " + ex.Message);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataTable = new DataTable();
+            LoadCurrentTable(); 
+        }
+        private void BtnFirst_Click(object sender, EventArgs e)
+        {
+            currentTableIndex = 0;
+            LoadCurrentTable();
+        }
+        private void BtnLast_Click(object sender, EventArgs e)
+        {
+            currentTableIndex = 4;
+            LoadCurrentTable();
+        }
+        private void BtnPrev_Click(object sender, EventArgs e)
+        {
+            if (currentTableIndex > 0)
+            {
+                currentTableIndex--;
+                LoadCurrentTable();
+            }
+        }
+
+        private void BtnNext_Click(object sender, EventArgs e)
+        {
+            if (currentTableIndex < tableNames.Count - 1)
+            {
+                currentTableIndex++;
+                LoadCurrentTable();
+            }
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            LoadCurrentTable();
+        }
+        private void BtnEdit_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null)
+            {
+                MessageBox.Show("Оберіть рядок для редагування.");
+                return;
+            }
+
+            string tableName = tableNames[currentTableIndex];
+            List<string> columns = tableColumns[tableName];
+            string primaryKey = tablePrimaryKeys[tableName];
+            object keyValue = dataGridView1.CurrentRow.Cells[primaryKey].Value;
+
+            string updateQuery = $"UPDATE {tableName} SET " +
+                string.Join(", ", columns.Where(c => c != primaryKey).Select(c => $"{c} = @{c}")) +
+                $" WHERE {primaryKey} = @{primaryKey}";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(updateQuery, conn))
+            {
+                try
+                {
+                    conn.Open();
+
+                    DataTable schemaTable = conn.GetSchema("Columns", new[] { null, null, tableName, null });
+                    Dictionary<string, string> columnTypes = new Dictionary<string, string>();
+                    foreach (DataRow row in schemaTable.Rows)
+                    {
+                        columnTypes.Add(row["COLUMN_NAME"].ToString(), row["DATA_TYPE"].ToString());
+                    }
+
+                    foreach (var col in columns)
+                    {
+                        object oldValue = dataGridView1.CurrentRow.Cells[col].Value;
+                        object newValue = PromptForValue(col, oldValue);
+
+                        if (newValue != null && columnTypes.ContainsKey(col))
+                        {
+                            string dataType = columnTypes[col];
+                            SqlParameter param = new SqlParameter("@" + col, SqlDbType.NVarChar);
+
+                            switch (dataType.ToLower())
+                            {
+                                case "int":
+                                    int intValue;
+                                    if (int.TryParse(newValue.ToString(), out intValue))
+                                        param = new SqlParameter("@" + col, SqlDbType.Int) { Value = intValue };
+                                    else
+                                        throw new Exception($"Значення для {col} має бути цілим числом.");
+                                    break;
+                                case "decimal":
+                                case "numeric":
+                                case "money":
+                                    decimal decValue;
+                                    if (decimal.TryParse(newValue.ToString(), out decValue))
+                                        param = new SqlParameter("@" + col, SqlDbType.Decimal) { Value = decValue };
+                                    else
+                                        throw new Exception($"Значення для {col} має бути числом з крапкою.");
+                                    break;
+                                case "datetime":
+                                case "date":
+                                    DateTime dateValue;
+                                    if (DateTime.TryParse(newValue.ToString(), out dateValue))
+                                        param = new SqlParameter("@" + col, SqlDbType.DateTime) { Value = dateValue };
+                                    else
+                                        throw new Exception($"Значення для {col} має бути датою у форматі ДД.ММ.РРРР.");
+                                    break;
+                                default:
+                                    param = new SqlParameter("@" + col, SqlDbType.NVarChar) { Value = newValue };
+                                    break;
+                            }
+
+                            cmd.Parameters.Add(param);
+                        }
+                        else
+                        {
+                            cmd.Parameters.AddWithValue("@" + col, newValue ?? DBNull.Value);
+                        }
+                    }
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Дані успішно оновлено.");
+                    LoadCurrentTable();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Помилка при редагуванні: " + ex.Message);
+                }
+            }
+        }
+        private object PromptForValue(string columnName, object defaultValue = null)
+        {
+            Form inputForm = new Form();
+            inputForm.Width = 300;
+            inputForm.Height = 150;
+            inputForm.FormBorderStyle = FormBorderStyle.FixedDialog;
+            inputForm.Text = $"Введення значення для {columnName}";
+            inputForm.StartPosition = FormStartPosition.CenterScreen;
+            inputForm.MaximizeBox = false;
+            inputForm.MinimizeBox = false;
+
+            Label label = new Label();
+            label.Text = $"Введіть значення для {columnName}:";
+            label.Left = 10;
+            label.Top = 20;
+            label.Width = 265;
+            inputForm.Controls.Add(label);
+
+            TextBox textBox = new TextBox();
+            textBox.Left = 10;
+            textBox.Top = 50;
+            textBox.Width = 265;
+            if (defaultValue != null && defaultValue != DBNull.Value)
+            {
+                textBox.Text = defaultValue.ToString();
+            }
+            inputForm.Controls.Add(textBox);
+
+            Button okButton = new Button();
+            okButton.Text = "OK";
+            okButton.Left = 120;
+            okButton.Top = 80;
+            okButton.Width = 60;
+            okButton.DialogResult = DialogResult.OK;
+            inputForm.Controls.Add(okButton);
+
+            inputForm.AcceptButton = okButton;
+
+            if (inputForm.ShowDialog() == DialogResult.OK)
+            {
+                return string.IsNullOrWhiteSpace(textBox.Text) ? null : textBox.Text;
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            string tableName = tableNames[currentTableIndex];
+            List<string> columns = tableColumns[tableName];
+            List<string> parameters = columns.Select(c => "@" + c).ToList();
+
+            string insertQuery = $"INSERT INTO {tableName} ({string.Join(", ", columns)}) VALUES ({string.Join(", ", parameters)})";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(insertQuery, conn))
+            {
+                foreach (var col in columns)
+                {
+                    object newValue = PromptForValue(col);
+                    cmd.Parameters.AddWithValue("@" + col, newValue ?? DBNull.Value);
+                }
+
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Запис успішно додано.");
+                    LoadCurrentTable();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Помилка при додаванні: " + ex.Message);
+                }
+            }
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null)
+            {
+                MessageBox.Show("Оберіть рядок для видалення.");
+                return;
+            }
+
+            DialogResult result = MessageBox.Show("Ви впевнені, що хочете видалити цей запис?",
+                                                "Підтвердження видалення",
+                                                MessageBoxButtons.YesNo,
+                                                MessageBoxIcon.Warning);
+
+            if (result != DialogResult.Yes)
+                return;
+
+            string tableName = tableNames[currentTableIndex];
+            string primaryKey = tablePrimaryKeys[tableName];
+            object keyValue = dataGridView1.CurrentRow.Cells[primaryKey].Value;
+
+            string deleteQuery = $"DELETE FROM {tableName} WHERE {primaryKey} = @{primaryKey}";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(deleteQuery, conn))
+            {
+                cmd.Parameters.AddWithValue("@" + primaryKey, keyValue);
+
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Запис успішно видалено.");
+                    LoadCurrentTable();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Помилка при видаленні: " + ex.Message);
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
